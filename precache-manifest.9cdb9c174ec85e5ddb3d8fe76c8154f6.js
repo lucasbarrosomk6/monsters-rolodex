7087fe9359819152702441cafd1facf0068e6c56
lucasbarrosomk6 <lucasbarrosomk6@gmail.com>
2019-12-03 18:34:18 -0500

    Updates

---
+++ precache-manifest.9cdb9c174ec85e5ddb3d8fe76c8154f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ed2a3a6d7d10bd2338291408a5a9b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "89736bdee8122a278c7b",
+    "url": "/monsters-rolodex/static/css/main.3f4237d5.chunk.css"
+  },
+  {
+    "revision": "00972e35ac24ba000a47",
+    "url": "/monsters-rolodex/static/js/2.2d55fda8.chunk.js"
+  },
+  {
+    "revision": "89736bdee8122a278c7b",
+    "url": "/monsters-rolodex/static/js/main.8d9bf30a.chunk.js"
+  },
+  {
+    "revision": "d4f9cf5c493fb869d5f4",
+    "url": "/monsters-rolodex/static/js/runtime-main.94c09bfb.js"
+  }
+]);
